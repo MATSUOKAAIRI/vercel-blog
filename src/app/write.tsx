097e0b26f4dbd1b465f74b8ce4995cf83a619eac
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-24 23:44:43 +0900

    インシデントの変更

--- src/app/write.tsx
+++ src/app/write.tsx
@@ -29,19 +29,20 @@ export default function Write() {
                 <div>
                     <label htmlFor="title">タイトル</label>
                     <Input
-          className={clsx(
+        className={clsx(
             'mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
             'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-          )}
+        )}
         />
                 </div>
                 <div>
                     <label htmlFor="content">内容</label>
                     <Input
-          className={clsx(
+        className={clsx(
             'mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
             'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-          )}
+    )
+}
         />
                 </div>
                 <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white" >投稿</Button>
