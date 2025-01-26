987f98771aa7165edaea73f38e7ca9002bbd8b40
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-26 23:50:02 +0900

    useEffect

--- src/app/write.tsx
+++ src/app/write.tsx
@@ -1,11 +1,13 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { Button , Input} from '@headlessui/react';
 import clsx from 'clsx';
+import { useRouter } from "next/navigation";
 
 export default function Write() {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const router = useRouter();
 
     const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTitle(e.target.value);
@@ -15,11 +17,12 @@ export default function Write() {
         setContent(e.target.value);
     };
 
+    useEffect(() => {
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         // ここでブログの内容を保存する処理を追加します
         console.log('タイトル:', title);
-        console.log('内容:', content);
+        console.log('内容:', content)}})
     };
 
     return (
@@ -28,11 +31,11 @@ export default function Write() {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="title">タイトル</label>
-                    <Input
+                    <Input onChange={(e) => setTitle(e.target.value)}
         className={clsx(
             'mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
             'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-        )}
+        ) }
         />
                 </div>
                 <div>
@@ -45,8 +48,9 @@ export default function Write() {
 }
         />
                 </div>
-                <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white" >投稿</Button>
+                <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white" 
+                disabled onClick={handleSubmit}>投稿</Button>
             </form>
         </div>
-    );
-}
+        
+    )
