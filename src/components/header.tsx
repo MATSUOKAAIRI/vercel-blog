3a9090430bf6e39200f79a30a3aa1c1104a17d30
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-28 23:23:46 +0900

    headerのタグの変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
     <header className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0">
-      <a href="/" className="text-2xl font-bold text-white">
+      <link href="/" className="text-2xl font-bold text-white">
         Vercel Blog
-      </a>
+      </link>
       <nav>
         
       </nav>
