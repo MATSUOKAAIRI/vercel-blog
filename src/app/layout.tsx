eb7904684c16cc7bd8a5481f53257c5d65823ca0
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-29 11:50:13 +0900

    wite.tsxの変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Write from "./write";
 
 export const metadata: Metadata = {
   title: "rii_blog",
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="ja">
       <body>
         <Header/>
+        <Write/>
         {children}
         <Footer/>
       </body>
---
+++ src/app/write.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React, { useState } from 'react';
+import clsx from 'clsx';
+
+export default function Write() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+
+    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+    };
+
+    const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setContent(e.target.value);
+    };
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setTitle('');
+        setContent('');
+    };
+    return (
+        <div>
+            <h1>記入してください</h1>
+            <form onSubmit={handleSubmit}>
+            <div>
+                    <label htmlFor="title">タイトル</label>
+                    <input 
+                        type="text" 
+                        id="title" 
+                        value={title} 
+                        onChange={handleTitleChange} 
+                        className={clsx(
+                            'mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
+                            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                        )}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="content">内容</label>
+                    <textarea 
+                        id="content" 
+                        value={content} 
+                        onChange={handleContentChange} 
+                        className={clsx(
+                            'mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
+                            'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                        )}
+                    />
+                </div>
+                <button 
+                    type="submit"
+                    className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+                >
+                    投稿
+                </button>
+            </form>
+        </div>
+        
+    )
+}
